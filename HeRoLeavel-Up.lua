311a547f65dd298097cc065b4ec593e294c45054
HeRoBaNd <belyaevroma@yandex.ru>
2015-12-29 20:41:30 +0300

    HeRoLeavel-Up.lua

--- HeRoLeavel-Up.lua
+++ HeRoLeavel-Up.lua
@@ -6,7 +6,7 @@
 ---1.3 - Added on ScriptStatus---
 ---1.4 - Add New Taric and Yorick logic---
 ---1.5 - Added AutoUpdater (Credit - Simple)---
-version = "1.5"
+version = "1.6"
 Sequences = {
 	[0]					=	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	[1]					=	{1, 2, 3, 1, 1, 4, 1, 2, 1, 2, 4, 2, 2, 3, 3, 4, 3, 3}, 
