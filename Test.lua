356df1abe0f17133a65c4749c0a0c6e0096c71ab
HeRoBaNd <belyaevroma@yandex.ru>
2016-01-26 21:23:35 +0300

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,13 +1,14 @@
 if myHero.charName ~= "JarvanIV" then return end
 
+local version = "1.0"
 local SCRIPT_NAME = "HeRo Jarvan"
 local SOURCELIB_URL = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
 local SOURCELIB_PATH = LIB_PATH.."SourceLib.lua"
 if FileExist(SOURCELIB_PATH) then
     require("SourceLib")
 else
-	DOWNLOADING_SOURCELIB = true
-	DownloadFile(SOURCELIB_URL, SOURCELIB_PATH, function() print("Required libraries downloaded successfully, please reload") end)
+    DOWNLOADING_SOURCELIB = true
+    DownloadFile(SOURCELIB_URL, SOURCELIB_PATH, function() print("Required libraries downloaded successfully, please reload") end)
 end
 
 if DOWNLOADING_SOURCELIB then 
@@ -35,7 +36,7 @@ function OnLoad()
 	EnemyMinions = minionManager(MINION_ENEMY, 700, myHero, MINION_SORT_MAXHEALTH_DEC)
 	jungleMinions = minionManager(MINION_JUNGLE, 700, myHero, MINION_SORT_MAXHEALTH_DEC)
 	Variables()
-	ts = TargetSelector(TARGET_LESS_CAST, 900, DAMAGE_PHYSICAL)
+	ts = TargetSelector(TARGET_LESS_CAST, 870, DAMAGE_PHYSICAL)
 	
 	HMenu = scriptConfig("Hero Jarvan", "Hero Jarvan")
 	VP = VPrediction()
@@ -43,7 +44,7 @@ function OnLoad()
 	HMenu:addSubMenu("[HeRo Jarvan - Combo]", "Combo")
 	HMenu.Combo:addParam("ComboMode", "Combo mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
 	HMenu.Combo:addParam("ComboEQ", "Use Q+E in Combo", SCRIPT_PARAM_ONOFF, true)
-	HMenu.Combo:addParam("ComboSaveEQ", "Always save E+Q for EQ combo", SCRIPT_PARAM_ONOFF, false)
+	HMenu.Combo:addParam("ComboSaveEQ", "Always save E+Q for Combo mode", SCRIPT_PARAM_ONOFF, false)
 	HMenu.Combo:addParam("ComboQ", "Use Q in Combo", SCRIPT_PARAM_ONOFF, true)
 	HMenu.Combo:addParam("ComboW", "Use W in Combo", SCRIPT_PARAM_ONOFF, true)
 	HMenu.Combo:addParam("ComboE", "Use E in Combo", SCRIPT_PARAM_ONOFF, true)
@@ -82,6 +83,7 @@ function OnLoad()
 	end
 	
 	HMenu:addSubMenu("[HeRo Jarvan - Drawings]", "Drawings")
+	HMenu.Drawings:addParam("AllDraw", "Enable or Disable all Draws", SCRIPT_PARAM_ONOFF, true)
 	HMenu.Drawings:addParam("DrawAA", "Draw AA Range", SCRIPT_PARAM_ONOFF, true)
 	HMenu.Drawings:addParam("DrawQ", "Draw Q Range", SCRIPT_PARAM_ONOFF, true)	
 	HMenu.Drawings:addParam("DrawW", "Draw W Range", SCRIPT_PARAM_ONOFF, true)
@@ -91,8 +93,40 @@ function OnLoad()
 	HMenu:addSubMenu("[HeRo Jarvan - Target Selector]", "targetSelector")
 	HMenu.targetSelector:addTS(ts)
 	ts.name = "Focus"
-		
-	print("<font color = \"#33CCCC\">HeRo Jarvan by</font> <font color = \"#fff8e7\">HeRoBaNd Loaded.</font>")
+
+	--[[IDPerma = HMenu:permaShow("ComboMode")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Combo Mode]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+
+	IDPerma = HMenu:permaShow("Harass")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Harass]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+	
+	IDPerma = HMenu:permaShow("LaneClear")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Lane Clear]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+	
+	IDPerma = HMenu:permaShow("JungleClear")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Jungle Clear]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+
+	IDPerma = HMenu:permaShow("ComboSaveEQ")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Always save E+Q for Combo mode]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+	
+	IDPerma = HMenu:permaShow("AutoUlt")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Use Auto Ultimate]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)	
+
+	IDPerma = HMenu:permaShow("minR")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Auto Ultimate if can hit X enemies]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+
+	IDPerma = HMenu:permaShow("ALLIN")
+	HMenu.permaShowEdit(IDPerma, "lText", "[Enable AutoKill]")
+	HMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+]]--
+	DelayAction(function() PrintChat("HeRo Jarvan Version " ..version.. " Loaded.") end, 4)
 end
 
 function Variables()
@@ -110,24 +144,8 @@ function OnTick()
 	spell_check()
 	ts:update()
 	
-	if HMenu.ComboMode then
+	if HMenu.Combo.ComboMode then
 		JarvanCombo()
-	end
-	
-	if HMenu.EQEscape then
-		mousePosEQ()
-	end
-	
-	if HMenu.LaneClearQ and HMenu.LaneClear then
-		LCLR()
-	end
-	
-	if HMenu.jungleclear then
-		JungleClear()
-	end
-	
-	if HMenu.Harass then
-		UseHarass()
 	end
 	
 	if UltActive then
@@ -135,9 +153,6 @@ function OnTick()
 			CastSpell(_R)
 		end
 	end
-	
-	killsteal()
-	autokill()
 	
 	if HMenu.AutoUlt then AutoUlt() end
 	
@@ -148,15 +163,6 @@ function spell_check()
 	Wready = (myHero:CanUseSpell(_W) == READY)
 	Eready = (myHero:CanUseSpell(_E) == READY)
 	Rready = (myHero:CanUseSpell(_R) == READY)
-end
-
-function UseHarass()
-	for i, target in pairs(GetEnemyHeroes()) do
-		local CastPosition, HitChance, Position = VP:GetLineCastPosition(target, 0.5, 70, 700, 1000, myHero, false)
-			if target ~= nil and Qready and ValidTarget(target) and HitChance >= 2 and GetDistance(CastPosition) < 700 then
-				CastSpell(_Q, CastPosition.x, CastPosition.z)
-		end
-	end
 end
 
 function GetDamageSpell()
@@ -205,39 +211,6 @@ function GetDamageSpell()
 	end
 	return RDamage
 end
-	
-
-function killsteal()
-local target = ts.target
-	for i, enemy in ipairs (GetEnemyHeroes()) do
-			local CastPosition, HitChance, Position = VP:GetLineCastPosition(enemy, 0.5, 70, 700, 1000, myHero, false)
-				if HMenu.KillSteal.QSteal and Qready and enemy ~= nil and not enemy.dead and enemy.health < QDamage and GetDistance(CastPosition) < 700 then
-					CastSpell(_Q, pos.x, pos.z)
-					end
-					end
-					
-	for i, enemy in ipairs (GetEnemyHeroes()) do
-			local CastPosition, HitChance, Position = VP:GetCircularCastPosition(target, 0.5, 70, 800, 1000)
-				if HMenu.KillSteal.ESteal and Eready and enemy ~= nil and not enemy.dead and enemy.health < EDamage and GetDistance(CastPosition) < 830 then
-					CastSpell(_E, pos.x, pos.z)
-					end
-					end
-					
-  local target = ts.target
-  for i, target in pairs (GetEnemyHeroes()) do
-			if HMenu.KillSteal.RSteal and target ~= nil and ValidTarget(target, 650) and not targer.dead and target.health < RDamage and GetDistance(CastPosition) < 650 then
-					CastSpell(_R, target)
-					end
-					end
-end
-
-function autokill()
-	local target = ts.target
-	if HMenu.ALLIN and Eready and Qready and Rready and not target.dead and target.health < QDamage + EDamage + RDamage then
-		ComboEQ()
-		ComboR()
-	end
-end
 
 function AutoUlt()
 local target = ts.target
@@ -260,105 +233,6 @@ function CountEnemies(range, unit)
 	return Enemies
 end
 
-function JarvanCombo()
-	if HMenu.Combo.ComboEQ then
-		ComboEQ()
-	end
-
-	if HMenu.Combo.ComboQ then
-		ComboQ()
-	end
-	
-	if HMenu.Combo.ComboE then
-		ComboE()
-	end
-	
-	if HMenu.Combo.comboW then
-		ComboW()
-	end
-	
-	if HMenu.Combo.comboR then
-		ComboR()
-	end
-end
-
-function ComboR()
-	local target = ts.target
-	if HMenu.Combo.combo then
-		for i, target in pairs(GetEnemyHeroes()) do
-			if target ~= nil and ValidTarget(target, 650) and not ultActive then
-				if blCheck(target) then
-						CastSpell(_R, target)
-					end
-				end
-			end
-		end
-	end
-
-function ComboQ()
-local target = ts.target
-	for i, target in pairs(GetEnemyHeroes()) do
-		local CastPosition, HitChance, Position = VP:GetCircularCastPosition(target, 0.5, 70, 770, 1000)
-		if target ~= nil and Qready and ValidTarget(target, 770) and HitChance >= 2 then
-			CastSpell(_E, CastPosition.x, CastPosition.z)
-				DelayAction(function() CastSpell(_Q, CastPosition.x, CastPosition.z) end, 0.2)
-			end
-		end
-	end
-
-	
-function ComboE()
-local target = ts.target
-	for i, target in pairs(GetEnemyHeroes()) do
-		local CastPosition, HitChance, Position = VP:GetCircularCastPosition(target, 0.5, 70, 800, 1000)
-			if target ~= nil and Eready and ValidTarget(target) and HitChance >= 2 and GetDistance(CastPosition) < 830 then
-				CastSpell(_E, CastPosition.x, CastPosition.z)
-		end
-	end
-end
-
-	
-function ComboW()
-	if CountEnemyHeroInRange(280) >= 1 then
-		CastSpell(_W)
-	end
-end
-
-function ComboEQ()
-local target = ts.target
-	if HMenu.Combo.ComboMode then
-		if HMenu.Combo.ComboSaveEQ then
-			for i, target in pairs(GetEnemyHeroes()) do
-				local CastPosition, HitChance, Position = VP:GetCircularCastPosition(target, 0.5, 70, 770, 1000)
-				if target ~= nil and Qready and Eready and ValidTarget(target, 770) and HitChance >= 2 then
-					CastSpell(_E, CastPosition.x, CastPosition.z)
-					DelayAction(function() CastSpell(_Q, CastPosition.x, CastPosition.z) end, 0.2)
-				end
-			end
-		end
-	else
-		for i, target in pairs(GetEnemyHeroes()) do
-			local CastPosition, HitChance, CastPosition = VP:GetCircularCastPosition(target, 0.5, 70, 770, 1000)
-			if target ~= nil and ValidTarget(target, 770) and HitChance >= 2 then
-				CastSpell(_E, CastPosition.x, CastPosition.z)
-				DelayAction(function() CastSpell(_Q, CastPosition.x, CastPosition.z) end, 0.2)
-			end
-		end
-	end
-end
-
-function LCLR()
-		for i, minion in pairs(EnemyMinions.objects) do
-			if minion ~= nil and ValidTarget(minion, 700) then
-				local CastPosition, HitChance, Position = VP:GetLineCastPosition(minion, 0.5, 70, 700, 1000, myHero, false)
-				if HitChance >= 2 and Qready and GetDistance(CastPosition) < 700 and ManaCheck(myHero, HMenu.LaneClear.lclrMana) == false then
-					CastSpell(_Q, CastPosition.x, CastPosition.z)
-				end
-				CastSpell(_E, minion.x, minion.z)
-			end
-		end
-		end
-
 function ManaCheck(unit, ManaValue)
 	if unit.mana < (unit.maxMana * (ManaValue/100))
 	then return true
@@ -372,55 +246,6 @@ function HealthCheck(unit, HealthValue)
 	then return true
 	else
 		return false
-	end
-end
-
-function OnDraw()
-	if myHero.dead then return end
-	
-	if HMenu.Drawings.DrawQ and Qready then
-		DrawCircle(myHero.x, myHero.y, myHero.z, 700, ARGB(255, 0, 0, 80))
-	end	
-
-	if HMenu.Drawings.DrawW and Wready then
-		DrawCircle(myHero.x, myHero.y, myHero.z, 300, ARGB(255, 0, 0, 80))
-	end
-
-	if HMenu.Drawings.DrawE and Eready then
-		DrawCircle(myHero.x, myHero.y, myHero.z, 830, ARGB(255, 0, 0, 80))
-	end
-
-	if HMenu.Drawings.drawAA then
-		DrawCircle(myHero.x, myHero.y, myHero.z, 250, ARGB(255, 0, 0, 80))
-	end
-	
-	if HMenu.Drawings.drawCR and Rready then
-		DrawCircle(myHero.x, myHero.y, myHero.z, 650, ARGB(255, 0, 0, 80))
-	end
-end
-
-
-function JungleClear()
-	for i, jungleMinion in pairs(jungleMinions.objects) do
-		if jungleMinion ~= nil then
-			if Eready and Qready and HMenu.JungleClear.JungleClearQ and HMenu.JungleClear.JungleClearE then
-				if ValidTarget(jungleMinion, 830) then
-					CastSpell(_E, jungleMinion.x, jungleMinion.z)
-					CastSpell(_Q, jungleMinion.x, jungleMinion.z)
-				end
-			else
-				if HMenu.JungleClear.jungleclearQ and ValidTarget(jungleMinion, 700) and Qready then
-					CastSpell(_Q, jungleMinion.x, jungleMinion.z)
-				end
-				
-				if HMenu.JungleClear.jungleclearE and ValidTarget(jungleMinion, 830) and Eready then
-					CastSpell(_E, jungleMinion)
-				end
-			end
-			if HMenu.JungleClear.jungleclearW and ValidTarget(jungleMinion, 300) and Wready then
-				CastSpell(_W)
-			end
-		end
 	end
 end
 
@@ -447,3 +272,99 @@ if obj == nil then return end
 	DelayAction(function() ultActive = false end, 3.5)
 	end
 	end
+
+function JarvanCombo()
+	if HMenu.Combo.ComboEQ then
+		ComboEQ()
+	end
+
+	if HMenu.Combo.ComboQ then
+		ComboQ()
+	end
+	
+	if HMenu.Combo.ComboE then
+		ComboE()
+	end
+	
+	if HMenu.Combo.comboW then
+		ComboW()
+	end
+	
+	if HMenu.Combo.comboR then
+		ComboR()
+	end
+end
+
+function ComboEQ()
+	if not Eready and not Qready then return end
+	if ValidTarget(ts.target, 770) then
+		local CastPosition, HitChance, Position = VP:GetCircularCastPosition(ts.target, 0.5, 70, 870, 1000, myHero, false)
+		if HitChance >= 2 then
+			CastSpell(_E, CastPosition.x, CastPosition.z)
+			DelayAction(function() CastSpell(_Q, CastPosition.x, CastPosition.z) end, 0.35)
+		end
+	end
+end
+
+function ComboQ()
+	if HMenu.Combo.ComboSaveEQ or not Qready then return end
+	if ValidTarget(ts.target, 770) then
+		local CastPosition, HitChance, Position = VP:GetLineCastPosition(ts.target, 0.5, 70, 870, 1000, myHero, false)
+		if HitChance >= 2 then
+			CastSpell(_Q, CastPosition.x, CastPosition.z)
+		end
+	end
+end
+
+function ComboE()
+	if HMenu.Combo.ComboSaveEQ or not Eready then return end
+	if ValidTarget(ts.target, 770) then
+		local CastPosition, HitChance, Position = VP:GetCircularCastPosition(ts.target, 0.5, 70, 870, 1000, myHero, false)
+		if HitChance >= 2 then
+			CastSpell(_E, CastPosition.x, CastPosition.z)
+		end
+	end
+end
+
+function ComboW()
+if HMenu.Combo.ComboW or not Wready then return end
+	if CountEnemyHeroInRange(280) >= 1 then
+		CastSpell(_W)
+	end
+end
+
+function ComboR()
+if HMenu.Combo.ComboR then
+	if not Rready then return end
+	if ValidTarget(ts.target, 650) then
+		CastSpell(_R, ts.target)
+	end
+	end
+end
+
+function OnDraw()
+	if myHero.dead then return end
+
+	if HMenu.Drawings.AllDraw then
+	
+		if HMenu.Drawings.DrawQ and Qready then
+			DrawCircle(myHero.x, myHero.y, myHero.z, 700, ARGB(255, 0, 0, 80))
+		end	
+
+		if HMenu.Drawings.DrawW and Wready then
+			DrawCircle(myHero.x, myHero.y, myHero.z, 300, ARGB(255, 0, 0, 80))
+		end
+
+		if HMenu.Drawings.DrawE and Eready then
+			DrawCircle(myHero.x, myHero.y, myHero.z, 830, ARGB(255, 0, 0, 80))
+		end
+
+		if HMenu.Drawings.drawAA then
+			DrawCircle(myHero.x, myHero.y, myHero.z, 250, ARGB(255, 0, 0, 80))
+		end
+	
+		if HMenu.Drawings.drawCR and Rready then
+			DrawCircle(myHero.x, myHero.y, myHero.z, 650, ARGB(255, 0, 0, 80))
+		end
+	end
+end
