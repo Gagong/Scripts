ad21d388870275cd7d364a67908b94b4bee564d1
HeRoBaNd <belyaevroma@yandex.ru>
2016-01-26 17:59:50 +0300

    Update HeRoLeavel-Up.lua

--- HeRoLeavel-Up.lua
+++ HeRoLeavel-Up.lua
@@ -1,4 +1,14 @@
 ---Auto Leavel Up Developed by HeRoBaNd---
+--[[
+ _   _     ______      _                          _        _   _       
+| | | |    | ___ \    | |                        | |      | | | |      
+| |_| | ___| |_/ /___ | |     ___  __ ___   _____| |      | | | |_ __  
+|  _  |/ _ \    // _ \| |    / _ \/ _` \ \ / / _ \ |      | | | | '_ \ 
+| | | |  __/ |\ \ (_) | |___|  __/ (_| |\ V /  __/ |      | |_| | |_) |
+\_| |_/\___\_| \_\___/\_____/\___|\__,_| \_/ \___|_|       \___/| .__/ 
+                                                                | |    
+                                                                |_|  
+--]]																																
 ---Changelog---
 ---1.0 - Reliased For Bol---
 ---1.1 - Fix Punctuation errors---
@@ -9,7 +19,9 @@
 ---1.6 - Fixed Rek'Sai spam error---
 ---1.7 - 6.1 Updated---
 ---1.8 - BugSplat Fixed---
-local LocalVersion = "1.9"
+---1.9 - Fixed random lvlup of some Champion---
+---2.0 - Fixed some ColorBug---
+local LocalVersion = "2.0"
 local autoupdate = true 
 local serveradress = "raw.githubusercontent.com"
 local scriptadress = "/HeRoBaNd/Scripts/master"
@@ -158,12 +170,13 @@ LastLevel = 0;
 function OnLoad()
 
 FindUpdates()
-PrintChat("<font color='#FF0000'><b>[HeRo Leavel-UP] </b></font>".."<font color='#FF0000'><b>Loaded.</b></font>")
+
+    DelayAction(function() PrintChat("<font color='#FF0000'><b>[HeRo Leavel-UP] </b></font>".."<font color='#FF0000'><b>Loaded.</b></font>") end, 4.0)
 
 	HLUMainMenu = scriptConfig('HeRoLevel-UP', 'HLU_MAIN')
 	
-	HLUMainMenu:addParam('Enable', 'Enable', SCRIPT_PARAM_ONOFF, false)
-	HLUMainMenu:addParam('AlwaysON', 'Always ON', SCRIPT_PARAM_ONOFF, false)
+	HLUMainMenu:addParam('Enable', 'Enable', SCRIPT_PARAM_ONOFF, true)
+	HLUMainMenu:addParam('AlwaysON', 'Always ON', SCRIPT_PARAM_ONOFF, true)
 	HLUMainMenu:addParam('Info', '-----------------------------------------------------', SCRIPT_PARAM_INFO, "-------------")
 	HLUMainMenu:addParam('Mode', 'Mode:', SCRIPT_PARAM_LIST, 1, {'Auto', 'Manual'})
     HLUMainMenu:addParam('Level13', 'Level 1-3:', SCRIPT_PARAM_LIST, 1, {'Q-W-E',  'Q-E-W',  'W-Q-E',  'W-E-Q',  'E-Q-W',  'E-W-Q'})
@@ -171,8 +184,14 @@ PrintChat("<font color='#FF0000'><b>[HeRo Leavel-UP] </b></font>".."<font color=
 	
 	HLUMainMenu.Enable = false
 	
+    IDPerma = HLUMainMenu:permaShow("AlwaysON")
+    HLUMainMenu.permaShowEdit(IDPerma, "lText", "[HeRoLeavel-Up Active]")
+    HLUMainMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
+    IDPerma = HLUMainMenu:permaShow("Mode")
+    HLUMainMenu.permaShowEdit(IDPerma, "lText", "[HeRoLeavel-Up Mode]")
+    HLUMainMenu.permaShowEdit(IDPerma, "lTextColor", 0xFF00FF00)
 
-	PrintChat("<font color='#EE82EE'><b><font color='#FF0000'><b>[HeRo Leavel-UP]: </b></font>" ..myHero.charName.. "<font color='#00FF00'> confirmed!</font></font></b>")
+    DelayAction(function() PrintChat("<font color='#EE82EE'><b><font color='#FF0000'><b>[HeRo Leavel-UP]: </b></font>" ..myHero.charName.. "<font color='#00FF00'> confirmed!</font></font></b>") end, 4.0)
 	
 end
 
@@ -235,7 +254,7 @@ _G.LevelSpell = function(id)
 			PrintChat("<font color='#FF0000'><b>[HeRo Leavel-Up] </b></font>".."<font color='#FAEBD7'><b>Updating, don't press F9.</b></font>")
 			Update()
 			else
-			PrintChat("<font color='#FF0000'><b>[HeRo Leavel-Up] </b></font>".."<font color='#FAEBD7'><b>You have the latest version.</b></font>")
+			PrintChat("<font color='#FF0000'><b>[HeRo Leavel-Up] </b></font>".."<font color='#00BFFF'><b>You have the latest version.</b></font>")
 			end
 		else
 		PrintChat("<font color='#FF0000'><b>[HeRo Leavel-Up] </b></font>".."<font color='#FAEBD7'><b>An error occured, while updating, please reload.</b></font>")
