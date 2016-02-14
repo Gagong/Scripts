b3aade854adbafc157e0488f1c6c613a1834b6bd
HeRoBaNd <belyaevroma@yandex.ru>
2016-02-14 11:09:04 +0300

    HeRo Smite v1.8

--- HeRo Smite.lua
+++ HeRo Smite.lua
@@ -16,11 +16,12 @@
 ---1.5 - Added KillSteal---
 ---1.6 - Fixed KillSteal error---
 ---1.7 - Fixed KillSteal Error---
+---1.8 - Delete KillStealSmite(soon)---
 local RangeSmite = 560
 local KillSmiteDmg = function() return myHero.level * 8 + 20 end
 local serveradress = "raw.githubusercontent.com"
 local scriptadress = "/HeRoBaNd/Scripts/master"
-local LocalVersion = "1.7"
+local LocalVersion = "1.8"
 local autoupdate = true
 
 if myHero:GetSpellData(SUMMONER_1).name:find("summonersmite") then Smite = SUMMONER_1 elseif myHero:GetSpellData(SUMMONER_2).name:find("summonersmite") then Smite = SUMMONER_2 end
