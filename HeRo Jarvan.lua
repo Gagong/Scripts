273ce3a0a90f424bb59f3e086242d15996670e29
HeRoBaNd <belyaevroma@yandex.ru>
2016-07-05 16:43:11 +0300

    2.05

--- HeRo Jarvan.lua
+++ HeRo Jarvan.lua
@@ -7,9 +7,9 @@
 ]]--
 
 if myHero.charName ~= "JarvanIV" then return end
-local version = "2.04"
+local version = "2.05"
 local SCRIPT_NAME = "HeRo Jarvan VI"
-local SCRIPT_PATCH = '6.12'
+local SCRIPT_PATCH = '6.13'
 local SCRIPT_AUTHOR = "HeRoBaNd"
 local FONTAN = false
 local xBase = {["x"] = 406, ["z"] = 424}
@@ -63,7 +63,7 @@ LastLevel = 0;
 function OnLoad()
 --Credits SxTeam
  	local ToUpdate = {}
-    ToUpdate.Version = 2.04
+    ToUpdate.Version = 2.05
     ToUpdate.UseHttps = true
     ToUpdate.Host = "raw.githubusercontent.com"
     ToUpdate.VersionPath = "/HeRoBaNd/Scripts/master/HeRo%20Jarvan.version"
@@ -165,7 +165,7 @@ function OnLoad()
 		Menu.Others.Snow:addParam("ChangeShow", "Change PermaShow Color(Green)", SCRIPT_PARAM_ONOFF, true)
 		Menu.Others.Snow:addParam("info", "Reload this(2xF9)", SCRIPT_PARAM_INFO, "")
 		
-		if VIP_USER and (string.find(GetGameVersion(), 'Releases/6.12') ~= nil) then
+		if VIP_USER and (string.find(GetGameVersion(), 'Releases/6.13') ~= nil) then
 			Menu.Others:addSubMenu("[Auto level - Up]", "LVLUP")
 				Menu.Others.LVLUP:addParam("Enable", "Enable LVL-UP", SCRIPT_PARAM_ONOFF, false)
 				Menu.Others.LVLUP:addParam("Mod", "Mode:", SCRIPT_PARAM_LIST, 1, {"Auto", "Manual"})
@@ -359,7 +359,7 @@ function OnTick()
 		print("<font color='#FF0000'><b>[HeRo Jarvan]: </b></font><font color='#F0F8FF'><b>New Harass mode: </b></font> "..PrintHarassMode(msgg))
 	end
 
-	if VIP_USER and (string.find(GetGameVersion(), 'Releases/6.12') ~= nil) and Menu.Others.LVLUP.Enable then
+	if VIP_USER and (string.find(GetGameVersion(), 'Releases/6.13') ~= nil) and Menu.Others.LVLUP.Enable then
 		print("<font color='#FF0000'><b>[HeRo - Info]: </b></font><font color='#F0F8FF'><b>I'm fix Lvl-Up function soon, sry.</b></font>")
 		Menu.Others.LVLUP.Enable = false
 		--if (LastLevel < myHero.level) then
@@ -1342,7 +1342,7 @@ end
 
 if VIP_USER then
 	_G.LevelSpell = function(id)
-  	if (string.find(GetGameVersion(), 'Releases/6.12') ~= nil) then
+  	if (string.find(GetGameVersion(), 'Releases/6.13') ~= nil) then
 	local Offset =
 	{ 
 		[1] = 0xEC,
@@ -1351,10 +1351,10 @@ if VIP_USER then
 		[4] = 0x65
 	}
 
-	local p = CLoLPacket(0xB9)
-	p.vTable = 0x108B5C0
+	local p = CLoLPacket(0x118)
+	p.vTable = 0x10134FC
 	p:EncodeF(myHero.networkID)
-	p:Encode4(Offset[id])
+	p:Encode1(Offset[id])
 	p:Encode4(0x70707070)
 	p:Encode4(0x43434343)
 	p:Encode1(0x7A)
